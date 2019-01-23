1b8d4ed1fdc59c255fcc3c7ed6cd5dbae4bcf982
Nathan_home <nathan.gong95@gmail.com>
2019-01-22 22:14:08 -0800

    Analysis plots

--- make_moving_hist.py
+++ make_moving_hist.py
@@ -4,6 +4,7 @@ sys.path.insert(0,os.getcwd()+'/Modules/')
 import utils
 import models
 import numpy as np
+import matplotlib.pyplot as plt
 """ data map:
 0: head x
 1: head y
@@ -31,3 +32,52 @@ train_data=np.concatenate(train_data)
 s,_,_=train_data.shape
 train_data=np.reshape(train_data, (s,14))
 print(train_data.shape)
+data=[]
+for i in range(7):
+	data.append(train_data[:,i*2:(i+1)*2])
+speed=[]
+for i in range(7):
+	temp=[]
+	for j in range(1,data[i].shape[0]):
+		temp.append(((data[i][j,0]-data[i][j-1,0])**2+(data[i][j,1]-data[i][j-1,1])**2)**0.5)
+	speed.append(np.asarray(temp))
+print(speed[0].shape)
+titles=['head','l_hand','r_hand','l_elbow','r_elbow','l_shoulder','r_shoulder']
+ax=[]
+for i in range(7):
+	ax.append(plt.subplot(4,2,i+1))
+	plt.hist(speed[i], bins=np.linspace(0,15,100),rwidth=1)
+	ax[i].set_ylim([0,60000])
+	plt.xlabel('speed')
+	plt.ylabel('Number of Occurance')
+	plt.title(titles[i])
+plt.show()
+#################################################
+def average_speed(data,step):
+	down_sampled=[]
+	for i in range(7):
+		down_sampled.append(data[i][::step,:])
+	speed=[]
+	for i in range(7):
+		temp=[]
+		for j in range(1,down_sampled[i].shape[0]):
+			temp.append(((down_sampled[i][j,0]-down_sampled[i][j-1,0])**2+(down_sampled[i][j,1]-down_sampled[i][j-1,1])**2)**0.5)
+		speed.append(np.asarray(temp))
+	all_speed=[]
+	for i in range(7):
+		all_speed.append(float(sum(speed[i]))/len(speed[i]))
+	return all_speed
+speed=[]
+for i in range(1,15):
+	speed.append(average_speed(data,i))
+speed=np.asarray(speed)
+ax=[]
+for i in range(7):
+	ax.append(plt.subplot(4,2,i+1))
+	plt.plot(speed[:,i])
+	ax[i].set_ylim([0,6])
+	plt.xlabel('Step Size')
+	plt.ylabel('Average_speed')
+	plt.title(titles[i])
+plt.show()
+print(speed.shape)
